28e7814510c680caeaff2fba195851b33c8a3439
Twansch <Twan_Schoenmakers@hotmail.com>
2017-01-17 12:06:16 +0100

    Update Garment.h conform Server

--- TestQT/Garment.h
+++ TestQT/Garment.h
@@ -1,35 +1,32 @@
-#ifndef GARMENT_H
-#define GARMENT_H
+#pragma once
 
-#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string.h>
 
 class Garment
 {
-public:
-    Garment(int id, double weight, std::string material, std::string color);
-    void SetDryer(bool dryer);
-    void SetSteamer(bool steamer);
-    void SetCentrifuge(bool centrifuge);
-    bool GetDryer();
-    bool GetSteamer();
-    bool GetCentrifuge();
-    int GetCustomerId();
-    double GetWeight();
-   // Color GetColor();
-  //  Material GetMaterial();
-
-
-
-
-private:
-    int id;
-    enum Material{Cotton, Wool, Satin, Nylon, Linen, Silk, Polyester };
-    enum Color{Black, White, Colored};
-    double weight;
-    bool dryer;
-    bool steamer;
-    bool centrifuge;
-    int customerId;
+	public:
+		Garment(int id, double weight, std::string material, std::string color);
+		void SetDryer(bool dryer);
+		void SetSteamer(bool steamer);
+		void SetCentrifuge(bool centrifuge);
+		int GetId() const;
+		double GetWeight() const ;
+		std::string GetMaterial() const;
+		std::string GetColor() const;
+		bool GetDryer() const;
+		bool GetSteamer() const;
+		bool GetCentrifuge() const;
+		
+	
+	private:
+		int id;
+		double weight;
+		std::string material;
+		std::string color;
+		bool dryer;
+		bool steamer;
+		bool centrifuge;
 };
-
-#endif // GARMENT_H
